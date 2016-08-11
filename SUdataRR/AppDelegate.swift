f60d997da91159ec76e914c063b8856ba2c6bb25
Daniel W. Anner <daniel.anner@danstechsupport.com>
2016-08-11 15:22:15 -0400

    initial commit

---
+++ SUdataRR/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SUdataRR
+//
+//  Created by danner on 8/11/16.
+//  Copyright © 2016 Daniel W. Anner. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SUdataRR/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SUdataRR
+//
+//  Created by danner on 8/11/16.
+//  Copyright © 2016 Daniel W. Anner. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
